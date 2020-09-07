fba12881613e2cb5ac5a1c1ecd192ed2e6bfb583
Jan Andrle <andrle.jan@centrum.cz>
2020-09-07 16:28:51 +0200

    utils – upsert

---
+++ src/loki_utils/transform_typedef.sub.js
@@ -0,0 +1,10 @@
+/**
+ * Předpřipravené sekvence úkonů pro databázi LokiJS (viz [JSDoc: Tutorial: Collection Transforms](http://techfort.github.io/LokiJS/tutorial-Collection%20Transforms.html)).
+ * 
+ * Lze používat:
+ * - [`*.transform(transform: **DB_TRANSFORMS**, parameters: **object**)`](http://techfort.github.io/LokiJS/Resultset.html#transform)
+ * - [*.chain(transform: **DB_TRANSFORMS**, parameters: **object**)](http://techfort.github.io/LokiJS/Collection.html#chain)
+ * @typedef {Object[]} DB_TRANSFORMACE
+ * @memberof db_utils
+ * @inner
+ */
---
+++ src/loki_utils/upsert.sub.js
@@ -0,0 +1,15 @@
+/**
+ * Pomocná funkce pro vložení/aktualizování záznamu v tabulce `colection`.
+ * @param {tb~TABLE} collection Cílová tabulka
+ * @param {object} query Argument pro {@link tb.findOne}
+ * @param {object} updated_data Aktualizovaná data
+ */
+function upsert(collection, query, updated_data){
+    const row= collection.findOne(query);
+    if(row){
+        collection.update(Object.assign(row, updated_data));
+        return 0;
+    }
+    collection.insert(updated_data);
+    return 1;
+}
