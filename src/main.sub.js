6bf998e6f795db8e5cd77bd07538279005ed45f2
Jan Andrle <andrle.jan@centrum.cz>
2020-09-07 16:24:59 +0200

    Načítání databáze + LokiWithUtils třída

---
+++ src/main.sub.js
@@ -0,0 +1,54 @@
+/* jshint maxdepth: 3 */
+/* global _dependencies */
+gulp_place("FSAdapter/*.sub.js", "glob_once");/* global FSAdapter */
+gulp_place('{ "glob": "loki_utils/*.sub.js", "use_strict": false }', "combine");/* global loki_utils */
+
+class LokiWithUtils extends _dependencies[0]{
+    constructor(file, params){
+        super(file, params);
+        if(this.utils) throw new Error("LokiWithUtils is not supported with current version of LokiJS!!!");
+        this.utils= loki_utils;
+    }
+    static create_(file, params){
+        var db;
+        return new Promise((resolve, reject)=> {
+            params.autoloadCallback= ()=> resolve(db);
+            try {
+                db= new this(file, params);
+            } catch (e){
+                reject(e);
+            }
+        });
+    }
+}
+
+export function database_({
+    db_file: { file= "db.json", prefix= "loki" }= {},
+    params= { autoload: true },
+    tables= [],
+    auto_cordova_adapter= true
+}= {}){
+    if(auto_cordova_adapter)
+        params.adapter= new FSAdapter({ prefix });
+    return LokiWithUtils.create_(file, params)
+        .then(function(db){
+            let tb= {};
+            let local_tables= db.listCollections().map(v=>v.name);
+            let actual_table= "", command= "", local_tables_index= -1;
+            for(let i= 0, i_length= tables.length; i < i_length; i++){
+                actual_table= tables[i];
+                command= "addCollection";
+                if(local_tables.length){
+                    local_tables_index= local_tables.indexOf(actual_table);
+                    if(local_tables_index!==-1){
+                        local_tables.splice(local_tables_index, 1);
+                        command= "getCollection";
+                    }
+                }
+                tb[actual_table]= db[command](actual_table);
+            }
+            for(let i=0, i_length= local_tables.length; i<i_length; i++){ db.removeCollection(local_tables[i]); }
+            db.saveDatabase();
+            return { db, tb };
+        });
+}
---
+++ src/wrapper.js
@@ -0,0 +1,2 @@
+/* jshint maxdepth: 3 */
+gulp_place('{ "name": "${app.name}", "file": "main.sub.js", "depends": { "loki": "loki" }, "type": "module" }', 'combine');
